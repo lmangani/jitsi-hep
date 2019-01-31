d95508ba5f5d6bbd017ecb47a2d77ee43a3ce5a4
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-01-31 18:17:42 +0100

    Update jitsi-hep.js

--- jitsi-hep.js
+++ jitsi-hep.js
@@ -5,19 +5,66 @@
     function Analytics(options) {
         /* eslint-disable */
         
+        /**
+         * Get the user IP throught the webkitRTCPeerConnection
+         * @param onNewIP {Function} listener function to expose the IP locally
+         * @return undefined
+         */
+        function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+            //compatibility for firefox and chrome
+            var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+            var pc = new myPeerConnection({
+                iceServers: []
+            }),
+            noop = function() {},
+            localIPs = {},
+            ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+            key;
+            function iterateIP(ip) {
+                if (!localIPs[ip]) onNewIP(ip);
+                localIPs[ip] = true;
+            }
+            //create a bogus data channel
+            pc.createDataChannel("");
+            // create offer and set local description
+            pc.createOffer().then(function(sdp) {
+                sdp.sdp.split('\n').forEach(function(line) {
+                    if (line.indexOf('candidate') < 0) return;
+                    line.match(ipRegex).forEach(iterateIP);
+                });
+                pc.setLocalDescription(sdp, noop, noop);
+            }).catch(function(reason) {
+                // An error occurred, so handle the failure to connect
+            });
+            //listen for candidate events
+            pc.onicecandidate = function(ice) {
+                if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+                ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+            };
+        }
+
+        // Tag Session
+        let session_ip = '0.0.0.0';
+        getUserIP(function(ip){
+          session_ip = ip;
+        });
          /**
          * HEP Analytics
          */
-          
+        var session = 'unknown';
         const hep = function(type,event,subset,data){
-            console.log('HEP-REQ',type,event,data);
+            // console.log('HEP-REQ',type,event,data);
+            // Attempt using device_id as Client identifier for statistics
+            if(data.attributes && data.attributes.device_id) session = data.attributes.device_id;
+            data.device_id = session;
+            data.ip = session_ip;
             // SHIP TO LOCAL/REMOTE COLLECTOR
              fetch('https://"+window.location.hostname||'hep.hepic.tel'+":9069', { // Your POST endpoint
                 method: 'POST',
                 mode: 'no-cors',
                 body: JSON.stringify(data) // This is your file object
               }).catch(
-                error => console.log(error) // Handle the error response object
+                // error => console.log(error) // Handle the error response object
               );      
         }
         window.hep = hep;
